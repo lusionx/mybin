e4c63a8679dbab419bebc2b9d57ff24b93fd895c
liuxing <lusionx@gmail.com>
2022-09-15 18:19:55 +0800

    rm data.json

--- asmr.mjs
+++ asmr.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 
 let { data: { files } } = require("./data.json")
-
+// eg. https://www.asmrgay.com/asmr/%E4%B8%AD%E6%96%87%E9%9F%B3%E5%A3%B0/jok
 
 for(const ff of files) {
     if(!ff.url) continue
